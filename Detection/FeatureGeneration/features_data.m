823aea1d1bd6f09eac8fb7ab91be298452bae757
Statistical Signal Processing Research Laboratory (SSPRL) <68095196+ssprl@users.noreply.github.com>
2020-08-20 13:28:52 -0500

    Script to generate real and imaginary features

--- Detection/FeatureGeneration/features_data.m
+++ Detection/FeatureGeneration/features_data.m
@@ -1,20 +1,37 @@
-% Siren_Noisy_Speech
+clear
+close all
+clc
 
-addpath('C:\Users\gxs160730\Documents\Ph.D\CNN-SE\CNN-VAD\Training Code\Functions\');
-addpath('C:\Users\gxs160730\Documents\Ph.D\Siren Noise\Features');
+%
+%  The script adds alert signal from thge directory to the noisy speech files in a directory.
+%
+%         Speech_path - Path for directory containing noisy speech files
+%         Siren_path  - Path for directory containing alert signal combined with noisy speech files
+%         dirname     - Path for saving  the .mat file
+%         fileName    - Name of the .mat file
+%
+%  Authors: Gautam Shreedhar Bhat
+%
+%  Copyright (c) 2019 by Gautam Shreedhar Bhat
+%------------------------------------------------------------------------------------
 
-Speech_path               = 'C:\Users\gxs160730\Documents\Ph.D\Siren Noise\Detection\Data\Clean\clean_part2';
-Siren_path                = 'C:\Users\gxs160730\Documents\Ph.D\Siren Noise\Detection\Data\Clean\clean_siren';
+Speech_path               = 'C:\Users\gxs160730\Documents\GitHub\Alert-signal-detector-and-separator\Detection\NoisySpeech\Traffic\Small_files\Part_1_test';
+Siren_path                = 'C:\Users\gxs160730\Documents\GitHub\Alert-signal-detector-and-separator\Detection\NoisySpeech\Traffic\Small_files\siren_files_test';
+dirname                   = 'C:\Users\gxs160730\Documents\GitHub\Alert-signal-detector-and-separator\Detection\NoisySpeech\Traffic\Features';
+fileName                  = 'Traffic_features';
 
 speechFiles               = dir(Speech_path);
 sirenFiles                = dir(Siren_path);
 
+%--- Fetch noise and speech files
 data.speechFiles          = speechFiles(~ismember({speechFiles.name},{'.' '..'}));
 data.sirenFiles           = sirenFiles(~ismember({sirenFiles.name},{'.' '..'}));
 
+%--- Number of speech and noise files
 data.nSpeechFiles         = numel(data.speechFiles);
 data.nSirenFiles          = numel(data.sirenFiles);
 
+%--- Initialize the parameters
 params.overlap           = 256;
 params.nfft              = 512;
 params.window            = hanning(params.nfft);
@@ -24,35 +41,26 @@ params.fmax              = 8000;
 params.fs                = 16000;
 params.filterbank        = buildFilterbank(params.fmin, params.fmax, params.n_mels, params.nfft, params.fs);
 
-cnt=1;cnt1=1;
-
-%trainData               = zeros(9374*data.nSirenFiles,28); 
-trainData                = zeros(11249*data.nSirenFiles,515); 
-
 for n = 1:data.nSpeechFiles
+    [speech,fs_si]         = audioread([data.speechFiles(n).folder '\' data.speechFiles(n).name]);
+    speech_ch1             = [speech(:,1);ones(length(speech),1)];
+    speech_ch2             = [speech(:,2);ones(length(speech),1)];
     
-    speech2                = [data.speechFiles(n).folder '\' data.speechFiles(n).name];
-    [speech1,fs_si]        = audioread(speech2);
-    
-    speech_ch1                 = [speech1(:,1);ones(length(speech1),1)];
-    speech_ch2                 = [speech1(:,2);ones(length(speech1),1)];    
     for m = n:n
         
-        siren2             = [data.sirenFiles(m).folder '\' data.sirenFiles(m).name];
-        [siren1,fs_si]     = audioread(siren2);
-        siren_ch1          = [siren1(:,1);zeros(length(siren1),1)];
-        siren_ch2          = [siren1(:,2);zeros(length(siren1),1)];
-            
+        [siren,fs_si]      = audioread([data.sirenFiles(m).folder '\' data.sirenFiles(m).name]);
+        siren_ch1          = [siren(:,1);zeros(length(siren),1)];
+        siren_ch2          = [siren(:,2);zeros(length(siren),1)];
         disp([m n])
         
-        A = [siren_ch1 speech_ch1 speech_ch1 siren_ch1 speech_ch1 siren_ch1 ];
+        %--- Concatenate audio
+        A = [siren_ch1 speech_ch1 speech_ch1 siren_ch1 speech_ch1 siren_ch1];
         B = A(:,randperm(size(A,2)));
-        C = B(1:length(speech1),:);
-        D = B(length(speech1)+1:end,:);
+        C = B(1:length(speech),:);
+        D = B(length(speech)+1:end,:);
+        C = reshape(C,[1,6*length(speech)]);
         
-        C = reshape(C,[1,6*length(speech1)]);
-        plot(C);
-        %% features
+        %--- Generate features
         audioFrames     = reshape(C, params.overlap, []);
         procFrames      = params.window .* [audioFrames(:,1:end-1); audioFrames(:,2:end)];
         procFFT         = fft(procFrames, params.nfft);
@@ -61,14 +69,20 @@ for n = 1:data.nSpeechFiles
         imag_ch1 = imag(procFFT(1:params.nfft/2+1,:));
         
         mag_procFFT     = abs(procFFT).^2;
-        STFTMdb1        = 10*log(mag_procFFT);        
-        STFTMdb         = STFTMdb1(1:params.nfft/2+1,:);
-                
-        %% labels
+        STFTMdb_tmp     = 10*log(mag_procFFT);
+        STFTMdb         = STFTMdb_tmp(1:params.nfft/2+1,:);
+        [len_rows, len_col] = size(procFrames);
+        
+        %--- Initialize matrix
+        if (n==1)
+            trainData  = zeros(len_col*data.nSirenFiles,(params.nfft/2+1) * 2 + 1);
+        end
+        
+        %--- Generate labels
         labelFrames     = reshape(D, params.overlap, []);
         procLabels      = [labelFrames(:,1:end-1); labelFrames(:,2:end)];
         len_1           = length(procLabels(1,:));
-                
+        
         for i = 1:len_1
             if (sum (procLabels(:,i) ) > 0)
                 Final_lables(:,i) = 0;
@@ -79,10 +93,10 @@ for n = 1:data.nSpeechFiles
         
         count = n;
         
-%         feature_vec = [melFeatures' Z Final_lables'];
         input = [real_ch1;imag_ch1];
         feature_vec = [input' Final_lables'];
         
+        %--- Fill the matrix trainData
         if count == 1
             trainData(1:count*len_1,:) = feature_vec;
             old_feature_vec = feature_vec;
@@ -90,9 +104,10 @@ for n = 1:data.nSpeechFiles
             trainData((count-1)*len_1+1:count*len_1,:) = feature_vec;
         end
     end
-   
+    
 end
 
-filename1 = strcat('C:\Users\gxs160730\Documents\Ph.D\Siren Noise\Detection\Data\Clean\Features\','clean_','RI_features');
-save(filename1,'trainData','-v7.3');
+%--- Write Features as .mat file
+feature_name = strcat(dirname,'\',fileName);
+save(feature_name,'trainData','-v7.3');
 disp('DONE!!!')
